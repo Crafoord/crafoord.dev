8210755abc1c8069b82aa6a22358f0a6382d802c
Crafoord <magnus.crafoord@gmail.com>
2021-03-19 14:55:25 +0100

    replace azure with gcp, run as container instead of static files (enable Image etc)

--- components/layout.tsx
+++ components/layout.tsx
@@ -29,29 +29,28 @@ export default function Layout({ children, home }: {children: ReactNode, home?: 
       <header className={styles.header}>
         {home ? (
           <>
-          <img src="/images/profile.jpg"></img>
-            {/* <Image
+            <Image
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
               alt={name}
-            /> */}
+            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
             <Link href="/">
               <a>
-                {/* <Image
+                <Image
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
                   alt={name}
-                /> */}
+                />
               </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
